a1e56663be4f84b2b20470e2ecb6ebad20a5f760
Vlad Balan <vlad.balan.dev@gmail.com>
2017-10-17 13:48:27 +0300

    Fix make_admin method

--- server/publications.js
+++ server/publications.js
@@ -119,8 +119,9 @@ Meteor.methods({
     check(user_id, String);
     // limit one admin
     if (Houston._admins.findOne({'user_id': {$exists: true}})) { return; }
-    Houston._admins.insert({user_id});
-    Houston._admins.insert({exists: true});
+    Houston._admins.insert({ user_id }); // TODO: verify if this is still necesary since we are using Roles now
+    Houston._admins.insert({ exists: true });
+    Roles.addUsersToRoles(user_id, ['admin']);
     sync_collections(); // reloads collections in case of new app
     return true;
   }
