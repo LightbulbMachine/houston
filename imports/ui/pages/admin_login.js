50d97af4a28497c2b0fc0af19758d2a9b285e2d8
Vlad Balan <vlad.balan.dev@gmail.com>
2017-10-23 16:44:38 +0300

    Display errors when clicking on "Claim Admin"

--- imports/ui/pages/admin_login.js
+++ imports/ui/pages/admin_login.js
@@ -8,6 +8,7 @@ import { validatePassword, validateLogin, validateEmail } from '../../util/valid
 class houston_login extends Component {
   constructor(props) {
     super(props);
+    this.afterLogin = this.afterLogin.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
     this.handleClaim = this.handleClaim.bind(this);
@@ -15,23 +16,24 @@ class houston_login extends Component {
     this.renderForm = this.renderForm.bind(this);
   }
 
+  afterLogin(error) {
+    const { history } = this.props;
+    // TODO error case that properly displays
+    if (error) {
+      return console.error(error);
+    } else {
+      history.push(Houston._ROOT_ROUTE);
+    }
+  };
+
   handleSubmit(e) {
     e.preventDefault();
     const { adminUserExists, history } = this.props;
     const email = this.email.value;
     const password = this.password.value;
 
-    const afterLogin = (error) => {
-      // TODO error case that properly displays
-      if (error) {
-        return console.error(error);
-      } else {
-        history.push(Houston._ROOT_ROUTE);
-      }
-    };
-
     if (adminUserExists) {
-      Meteor.loginWithPassword(email, password, afterLogin);
+      Meteor.loginWithPassword(email, password, this.afterLogin);
     } else {
       const user = { password };
 
@@ -51,8 +53,8 @@ class houston_login extends Component {
       }
 
       Accounts.createUser(user, (error) => {
-        if (error) { return afterLogin(error); }
-        Houston.becomeAdmin(afterLogin);
+        if (error) { return this.afterLogin(error); }
+        Houston.becomeAdmin(this.afterLogin);
       });
     }
   }
@@ -67,8 +69,7 @@ class houston_login extends Component {
 
   handleClaim(e) {
     e.preventDefault();
-    const { history } = this.props;
-    Houston.becomeAdmin(() => { history.push(Houston._ROOT_ROUTE) });
+    Houston.becomeAdmin(this.afterLogin);
   }
 
   renderHeading() {
